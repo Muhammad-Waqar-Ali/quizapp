6be29d274499d021606f107fdc3d8236f97b3f1f
Muhammad-Waqar-Ali <79845339+Muhammad-Waqar-Ali@users.noreply.github.com>
2021-06-16 18:27:45 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QuizContainer from './components/QuizContainer'
+
+function App() {
+  return (
+    <div>
+      <QuizContainer></QuizContainer>
+    </div>
+  );
+}
+
+export default App;
